0354b66d7332aec9a1cf71d89c3e1d30bde6c52a
John Cadungog <john.cadungog@cit.edu>
2023-11-20 08:12:35 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace ImageProcessingActivity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imageHistogram = new System.Windows.Forms.PictureBox();
+            this.orignalImage = new System.Windows.Forms.PictureBox();
+            this.processedImage = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.greyScaleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorInversionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sepiaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.showHistogram = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imageHistogram)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orignalImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.processedImage)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imageHistogram
+            // 
+            this.imageHistogram.Location = new System.Drawing.Point(531, 27);
+            this.imageHistogram.Name = "imageHistogram";
+            this.imageHistogram.Size = new System.Drawing.Size(279, 250);
+            this.imageHistogram.TabIndex = 0;
+            this.imageHistogram.TabStop = false;
+            // 
+            // orignalImage
+            // 
+            this.orignalImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.orignalImage.Location = new System.Drawing.Point(105, 315);
+            this.orignalImage.Name = "orignalImage";
+            this.orignalImage.Size = new System.Drawing.Size(311, 250);
+            this.orignalImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.orignalImage.TabIndex = 1;
+            this.orignalImage.TabStop = false;
+            // 
+            // processedImage
+            // 
+            this.processedImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.processedImage.Location = new System.Drawing.Point(499, 315);
+            this.processedImage.Name = "processedImage";
+            this.processedImage.Size = new System.Drawing.Size(311, 250);
+            this.processedImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.processedImage.TabIndex = 2;
+            this.processedImage.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(918, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.greyScaleToolStripMenuItem,
+            this.colorInversionToolStripMenuItem,
+            this.copyToolStripMenuItem1,
+            this.sepiaToolStripMenuItem});
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.copyToolStripMenuItem.Text = "Menu";
+            // 
+            // greyScaleToolStripMenuItem
+            // 
+            this.greyScaleToolStripMenuItem.Name = "greyScaleToolStripMenuItem";
+            this.greyScaleToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.greyScaleToolStripMenuItem.Text = "Grey Scale";
+            this.greyScaleToolStripMenuItem.Click += new System.EventHandler(this.greyScaleToolStripMenuItem_Click);
+            // 
+            // copyToolStripMenuItem1
+            // 
+            this.copyToolStripMenuItem1.Name = "copyToolStripMenuItem1";
+            this.copyToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.copyToolStripMenuItem1.Text = "Copy";
+            this.copyToolStripMenuItem1.Click += new System.EventHandler(this.copyToolStripMenuItem1_Click);
+            // 
+            // colorInversionToolStripMenuItem
+            // 
+            this.colorInversionToolStripMenuItem.Name = "colorInversionToolStripMenuItem";
+            this.colorInversionToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.colorInversionToolStripMenuItem.Text = "Color Inversion";
+            this.colorInversionToolStripMenuItem.Click += new System.EventHandler(this.colorInversionToolStripMenuItem_Click);
+            // 
+            // sepiaToolStripMenuItem
+            // 
+            this.sepiaToolStripMenuItem.Name = "sepiaToolStripMenuItem";
+            this.sepiaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sepiaToolStripMenuItem.Text = "Sepia";
+            this.sepiaToolStripMenuItem.Click += new System.EventHandler(this.sepiaToolStripMenuItem_Click);
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(192, 167);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(98, 34);
+            this.buttonLoad.TabIndex = 4;
+            this.buttonLoad.Text = "Load";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(192, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 34);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // showHistogram
+            // 
+            this.showHistogram.AutoSize = true;
+            this.showHistogram.Location = new System.Drawing.Point(192, 119);
+            this.showHistogram.Name = "showHistogram";
+            this.showHistogram.Size = new System.Drawing.Size(103, 17);
+            this.showHistogram.TabIndex = 6;
+            this.showHistogram.Text = "Show Histogram";
+            this.showHistogram.UseVisualStyleBackColor = true;
+            this.showHistogram.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(528, 280);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Histogram";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(496, 568);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Processed Image";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(102, 568);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Loaded Image";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(918, 593);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.showHistogram);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.processedImage);
+            this.Controls.Add(this.orignalImage);
+            this.Controls.Add(this.imageHistogram);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageHistogram)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orignalImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.processedImage)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imageHistogram;
+        private System.Windows.Forms.PictureBox orignalImage;
+        private System.Windows.Forms.PictureBox processedImage;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem greyScaleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem colorInversionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sepiaToolStripMenuItem;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.CheckBox showHistogram;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace ImageProcessingActivity
+{
+    public partial class Form1 : Form
+    {
+        Bitmap loadedImage;
+        Bitmap resultImage;
+        Bitmap histoImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Histogram(ref Bitmap a, ref Bitmap b)
+        {
+            Color sample;
+            Color gray;
+            Byte graydata;
+
+            // Grayscale Conversion
+            for (int x = 0; x < a.Width; x++)
+            {
+                for (int y = 0; y < a.Height; y++)
+                {
+                    sample = a.GetPixel(x, y);
+                    graydata = (byte)((sample.R + sample.G + sample.B) / 3);
+                    gray = Color.FromArgb(graydata, graydata, graydata);
+                    a.SetPixel(x, y, gray);
+                }
+            }
+
+            // Histogram 1D data
+            int[] histdata = new int[256]; 
+            for (int x = 0; x < a.Width; x++)
+            {
+                for (int y = 0; y < a.Height; y++)
+                {
+                    sample = a.GetPixel(x, y);
+                    histdata[sample.R]++; // can be any color property r, g, or b
+                }
+            }
+
+            // Bitmap Graph Generation
+            // Setting empty Bitmap with background color
+            b = new Bitmap(histdata.Length, 800);
+            for (int x = 0; x < histdata.Length; x++)
+            {
+                for (int y = 0; y < 800; y++)
+                {
+                    b.SetPixel(x, y, Color.White);
+                }
+            }
+
+            // Plotting points based on histdata
+            for (int x = 0; x < histdata.Length; x++)
+            {
+                for (int y = 0; y < Math.Min(histdata[x] / 5, b.Height - 1); y++)
+                {
+                    b.SetPixel(x, (b.Height - 1) - y, Color.Black);
+                }
+            }
+
+            imageHistogram.Image = b;
+        }
+
+
+        private void copyToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            resultImage = new Bitmap(loadedImage.Width, loadedImage.Height);
+            for (int x=0; x<resultImage.Width; x++)
+            {
+                for(int y=0; y<resultImage.Height; y++)
+                {
+                    Color pixel = loadedImage.GetPixel(x, y);
+                    resultImage.SetPixel(x, y, pixel);
+                }
+            }
+            processedImage.SizeMode = PictureBoxSizeMode.StretchImage;
+            processedImage.Image = resultImage;
+            UpdateHistogram();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+           // Histogram(ref loadedImage, ref histoImage);
+
+            
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            loadedImage = new Bitmap(openFileDialog1.FileName);
+            orignalImage.Image = loadedImage;
+
+
+        }
+
+        private void greyScaleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            resultImage = new Bitmap(loadedImage.Width, loadedImage.Height);
+            for (int x = 0; x < resultImage.Width; x++)
+            {
+                for (int y = 0; y < resultImage.Height; y++)
+                {
+                    Color pixel = loadedImage.GetPixel(x, y);
+                    int grey = (pixel.R + pixel.G + pixel.B) / 3;
+                    resultImage.SetPixel(x, y, Color.FromArgb(grey, grey, grey));
+                }
+            }
+            processedImage.SizeMode = PictureBoxSizeMode.StretchImage;
+            processedImage.Image = resultImage;
+            UpdateHistogram();
+        }
+
+        private void colorInversionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            resultImage = new Bitmap(loadedImage.Width, loadedImage.Height);
+            for (int x = 0; x < resultImage.Width; x++)
+            {
+                for (int y = 0; y < resultImage.Height; y++)
+                {
+                    Color pixel = loadedImage.GetPixel(x, y);
+                    resultImage.SetPixel(x, y, Color.FromArgb(255 - pixel.R, 255 - pixel.G, 255 - pixel.B)); 
+                }
+            }
+            processedImage.SizeMode = PictureBoxSizeMode.StretchImage;
+            processedImage.Image = resultImage;
+            UpdateHistogram();
+        }
+
+        private void sepiaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            resultImage = new Bitmap(loadedImage.Width, loadedImage.Height);
+            for (int x = 0; x < resultImage.Width; x++)
+            {
+                for (int y = 0; y < resultImage.Height; y++)
+                {
+                    Color pixel = loadedImage.GetPixel(x, y);
+
+                    int sepiaIntensity = 20;
+
+                    int newRed = (int)(pixel.R * 0.393 + pixel.G * 0.769 + pixel.B * 0.189);
+                    int newGreen = (int)(pixel.R * 0.349 + pixel.G * 0.686 + pixel.B * 0.168);
+                    int newBlue = (int)(pixel.R * 0.272 + pixel.G * 0.534 + pixel.B * 0.131);
+
+                    newRed = Math.Min(255, newRed + sepiaIntensity);
+                    newGreen = Math.Min(255, newGreen + sepiaIntensity);
+                    newBlue = Math.Min(255, newBlue + sepiaIntensity);
+
+                    resultImage.SetPixel(x, y, Color.FromArgb(newRed, newGreen, newBlue));
+                }
+            }
+            processedImage.SizeMode = PictureBoxSizeMode.StretchImage;
+            processedImage.Image = resultImage;
+            UpdateHistogram();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!showHistogram.Checked)
+            {
+                // Clear the histogram picture box
+                imageHistogram.Image = null; 
+            }
+            else
+            {
+                // Show the histogram
+                Histogram(ref resultImage, ref histoImage);
+            }
+        }
+        private void UpdateHistogram()
+        {
+            if (showHistogram.Checked)
+            {
+                Histogram(ref resultImage, ref histoImage);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
